f3d452bf0c41a3da8940596b3c3be475edf757db
Roman Kochetkov <romankochetkoveducation@gmail.com>
2023-04-16 21:51:24 +0700

    Add header file for input check

---
+++ src/lib/input_checker.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+struct point {
+    double x;
+    double y;
+};
+typedef struct point point;
+
+struct circle {
+    point center;
+    double radius;
+};
+typedef struct circle circle;
+
+void strToLower(char* str);
+
+int isArguments(char* str);
+
+int isLastBracket(char* str);
+
+int isObject(char* str);
+
+int printErrors(char* str, int countObj);
