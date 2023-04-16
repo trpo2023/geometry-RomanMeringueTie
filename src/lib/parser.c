5a639a983f6c2cbfad1350a402b66c44bbcefbae
Roman Kochetkov <romankochetkoveducation@gmail.com>
2023-04-16 21:54:49 +0700

    Add file for getting radius and center

---
+++ src/lib/parser.c
@@ -0,0 +1,39 @@
+#include <lib/parser.h>
+#include <lib/input_checker.h>
+
+int getCenter(char *str, circle *circle)
+{
+    char *number = malloc(10);
+    int bracket = 0;
+    for (int i = 0;; i++)
+    {
+        if (str[i] == '(')
+        {
+            bracket = i;
+            break;
+        }
+    }
+    int index = 0;
+    for (int i = bracket + 1; str[i] != ','; i++)
+        number[index++] = str[i];
+    if (sscanf(number, "%lf %lf", &(circle->center.x), &(circle->center.y)) == 2)
+        return 0;
+    else
+        return -1;
+}
+
+int getRadius(char *str, circle *circle)
+{
+    char *number = malloc(10);
+    int comma = 0;
+    for (int i = 0; str[i] != ','; i++)
+        comma = i + 2;
+    int index = 0;
+    for (int i = comma; str[i] != ')'; i++)
+        number[index++] = str[i];
+    if (sscanf(number, "%lf", &(circle->radius)) == 1)
+        return 0;
+    else
+        return -1;
+    return 0;
+}
