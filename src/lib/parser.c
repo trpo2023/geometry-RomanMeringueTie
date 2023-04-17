e8705c89857d2f86477718498c8a17753befd280
Roman Kochetkov <romankochetkoveducation@gmail.com>
2023-04-17 22:44:25 +0700

    Fix codestyle

--- src/lib/parser.c
+++ src/lib/parser.c
@@ -1,15 +1,15 @@
-#include <lib/parser.h>
 #include <lib/lexer.h>
+#include <lib/parser.h>
 #include <math.h>
 #define PI 3.14
 
-double perimeter(circle *circle)
+double perimeter(circle* circle)
 {
     double perimetr = 2 * PI * circle->radius;
     return perimetr;
 }
 
-double area(circle *circle)
+double area(circle* circle)
 {
     double area = PI * pow(circle->radius, 2);
     return area;
